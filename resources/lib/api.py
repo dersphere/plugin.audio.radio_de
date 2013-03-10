f23907424d2e6402b13d69d24a1220656ac05188
dersphere <sphere@dersphere.de>
2013-03-10 23:59:11 +0100

    changed from simplejson to json

--- resources/lib/api.py
+++ resources/lib/api.py
@@ -16,7 +16,7 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
-import simplejson as json
+import json
 from urllib import urlencode
 from urllib2 import urlopen, Request, HTTPError, URLError
 import random
